a4547bdbfe7c35b27c7c0d065f8495face3b9731
luturol <rafael.ahrons@gmail.com>
2016-07-17 17:07:54 -0300

    first commit

---
+++ src/main/java/io/tudonoob/HadesPacienteCrudApplication.java
@@ -0,0 +1,12 @@
+package io.tudonoob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HadesPacienteCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HadesPacienteCrudApplication.class, args);
+	}
+}
